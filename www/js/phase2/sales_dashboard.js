a8971aa3bf7e6156840c8a6cd41944696a87bde2
Kicky13 <kicky120@gmail.com>
2019-02-15 08:13:57 +0700

    Sales Dashboard 60%

--- www/js/phase2/sales_dashboard.js
+++ www/js/phase2/sales_dashboard.js
@@ -49,13 +49,39 @@ $(document).ready(function () {
 });
 
 function dataThisDay() {
-    var table = $('#dailyTable tbody');
     $.ajax({
         type: 'POST',
         dataType: 'JSON',
         url: api_url + '/sales_new/getSMIGThisDay'
     }).done(function (data) {
         console.log(data);
+        document.getElementById('ZAKVD').innerHTML = (data.perDay.ZAK.VOLUME/1).toFixed(2);
+        document.getElementById('ZAKPD').innerHTML = (data.perDay.ZAK.PRICE/1).toFixed(2);
+        document.getElementById('ZAKPRSD').innerHTML = 0;
+        document.getElementById('ZAKRD').innerHTML = (data.perDay.ZAK.REVENUE/1000000).toFixed(2);
+        document.getElementById('CRHVD').innerHTML = (data.perDay.Curah.VOLUME/1).toFixed(2);
+        document.getElementById('CRHPD').innerHTML = (data.perDay.Curah.PRICE/1).toFixed(2);
+        document.getElementById('CRHPRSD').innerHTML = 0;
+        document.getElementById('CRHRD').innerHTML = (data.perDay.Curah.REVENUE/1000000).toFixed(2);
+        document.getElementById('CLINKVD').innerHTML = (data.perDay.Clinker.VOLUME/1).toFixed(2);
+        document.getElementById('CLINKPD').innerHTML = (data.perDay.Clinker.PRICE/1).toFixed(2);
+        document.getElementById('CLINKPRSD').innerHTML = 0;
+        document.getElementById('CLINKRD').innerHTML = (data.perDay.Clinker.REVENUE/1000000).toFixed(2);
+        document.getElementById('KSOVD').innerHTML = (data.perOpco.SI.VOLUME/1).toFixed(2);
+        document.getElementById('KSOPD').innerHTML = (data.perOpco.SI.PRICE/1).toFixed(2);
+        document.getElementById('KSORD').innerHTML = (data.perOpco.SI.REVENUE/1000000).toFixed(2);
+        document.getElementById('SGVD').innerHTML = (data.perOpco.SG.VOLUME/1).toFixed(2);
+        document.getElementById('SGPD').innerHTML = (data.perOpco.SG.PRICE/1).toFixed(2);
+        document.getElementById('SGRD').innerHTML = (data.perOpco.SG.REVENUE/1000000).toFixed(2);
+        document.getElementById('SPVD').innerHTML = (data.perOpco.SP.VOLUME/1).toFixed(2);
+        document.getElementById('SPPD').innerHTML = (data.perOpco.SP.PRICE/1).toFixed(2);
+        document.getElementById('SPRD').innerHTML = (data.perOpco.SP.REVENUE/1000000).toFixed(2);
+        document.getElementById('STVD').innerHTML = (data.perOpco.ST.VOLUME/1).toFixed(2);
+        document.getElementById('STPD').innerHTML = (data.perOpco.ST.PRICE/1).toFixed(2);
+        document.getElementById('STRD').innerHTML = (data.perOpco.ST.REVENUE/1000000).toFixed(2);
+        document.getElementById('TLCCVD').innerHTML = (data.perOpco.TL.VOLUME/1).toFixed(2);
+        document.getElementById('TLCCPD').innerHTML = (data.perOpco.TL.PRICE/1).toFixed(2);
+        document.getElementById('TLCCRD').innerHTML = (data.perOpco.TL.REVENUE/100000g0).toFixed(2);
     }).always(function (data) {
         console.log(data);
         uploadControl(JSON.stringify(data), 'SalesDaily', 'Sales/Daily');
